1029802c6a8189cfaf3adfa7f8bce3210f2aa42b
iAyeBot <iayebot@websemantics.ca>
2016-09-05 16:10:15 -0400

    Deploy to gh-pages

---
+++ assets/js/src/application.js
@@ -0,0 +1,36 @@
+/**
+ *
+ *            :
+ *      . :   : :
+ *      ; . : ' : ;
+ *      : : : . : ;
+ *      ; . : ' : ;
+ *     ____________
+ *   _/_/_.______/ \
+ *  | \__________\_/
+ *  |______   _                         _     _
+ *       | \ | |__   ___ _ __ ___   ___| |__ | |_ __
+ *       | | | '_ \ / _ \ '_ ` _ \ / _ \ '_ \| | '__|
+ *       | | | | | |  __/ | | | | |  __/ |_) | | |
+ *       [_] |_| |_|\___|_| |_| |_|\___|_.__/|_|_|
+ *
+ *       Build Bootstrap Stuff the Right Way!
+ *
+ * This project was released under MIT license.
+ *
+ * @link      http://websemantics.ca
+ * @author    Web Semantics, Inc. Dev Team <team@websemantics.ca>
+ * @author    Adnan M.Sagar, PhD. <adnan@websemantics.ca>
+ *
+ */
+
+!function ($) {
+  'use strict';
+
+  $(function () {
+
+    // There is nothing here yet!
+
+  })
+
+}(jQuery)
